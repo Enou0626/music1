0073ffe377287da517f2c28b4dc361bd75a94718
zhouqing <1029654986@qq.com>
2018-12-24 20:12:52 +0800

    singer-detail transition

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -15,4 +15,12 @@ export default {};
   bottom: 0;
   width: 100%;
 }
+
+.slide-enter-active, .slide-leave-active {
+  transition: all 0.3s ease;
+}
+
+.slide-enter, .slide-leave-to {
+  transform: translate3d(100%, 0, 0);
+}
 </style>
